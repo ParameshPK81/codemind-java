d169786da9363efd692decbc3b67eb25e6fd7fdc
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-08-25 15:42:04 +0530

    import java.util.*; class j{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a,b,c;         double s;         double p;         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();
            s=(a+b+c)/2.0;
            p=(double)(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
            System.out.printf("%.2f",(float)p);
        }
    }

---
+++ Area_of_Triangle_.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class j{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        double s;
+        double p;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        
+        s=(a+b+c)/2.0;
+        p=(double)(Math.sqrt(s*(s-a)*(s-b)*(s-c)));
+        System.out.printf("%.2f",(float)p);
+    }
+}
