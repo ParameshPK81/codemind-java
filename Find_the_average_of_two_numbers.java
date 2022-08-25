346652a356ce336edbb3018a8a4cdaddae10a8a2
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-08-25 15:47:39 +0530

    import java.util.*; class c{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a,b;         a=sc.nextInt();         b=sc.nextInt();         double avg=(a+b)/2.0;         System.out.printf("%.4f",(float)avg);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class c{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        double avg=(a+b)/2.0;
+        System.out.printf("%.4f",(float)avg);
+    }
+}
