9d27001e454f87fe33d68ff244195c825c8961e6
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-08-25 15:56:15 +0530

    import java.util.*; class c{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         char c;         c=sc.next().charAt(0);         System.out.printf("%d",(int)c);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class c{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        char c;
+        c=sc.next().charAt(0);
+        System.out.printf("%d",(int)c);
+    }
+}
