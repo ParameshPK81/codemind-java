9770bbd40b4224bbd6bcdc224b814b3e7c5f5412
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-08-26 09:48:13 +0530

    import java.util.*; class y{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a,b,d;         a=sc.nextInt();         b=sc.nextInt();         d=a/b;         System.out.println(d);     } }

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class y{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a,b,d;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        d=a/b;
+        System.out.println(d);
+    }
+}
