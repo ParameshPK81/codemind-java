b909c01b18b0576326004a3626a3f53b976fbd77
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-09-09 11:12:11 +0530

    import java.util.*; class Pro{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         long a,b;         a=sc.nextInt();         b=sc.nextInt();         System.out.println(a+b);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Pro{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        long a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+    }
+}
