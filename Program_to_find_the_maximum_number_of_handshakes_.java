cd590f490cb4c03d4c44b5ffde30a06c1af703a6
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-09-09 11:16:58 +0530

    import java.util.*; class Pro{ public static void main(String args[]){     Scanner sc=new Scanner(System.in);     long n;     n=sc.nextInt();     System.out.printf("%d",(n*(n-1))/2);
        }
    }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Pro{
+public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    long n;
+    n=sc.nextInt();
+    System.out.printf("%d",(n*(n-1))/2);
+    
+    }
+}
