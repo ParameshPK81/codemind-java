3b47ec115921d2b328f7f4d6ee622de11a25b240
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-09-09 11:37:21 +0530

    import java.util.*; class k{     public static void  main(String[] args){         Scanner sc = new Scanner(System.in);         int a,b,k;         a=sc.nextInt();         b=sc.nextInt();         k=a%b;         System.out.printf("%d",k);     } }

---
+++ Modulus.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class k{
+    public static void  main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a,b,k;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        k=a%b;
+        System.out.printf("%d",k);
+    }
+}
