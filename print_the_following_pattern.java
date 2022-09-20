b0f4c913d999e23ab371f1c16d7706b7278d0efe
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-09-20 20:39:07 +0530

    import java.util.*; class Pro{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n;         n=sc.nextInt();         while(n!=0){             for(int i=1;i<=n;i++){                 System.out.printf("%d",i);             }             n--;             System.out.printf(" ");         }     } }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Pro{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        while(n!=0){
+            for(int i=1;i<=n;i++){
+                System.out.printf("%d",i);
+            }
+            n--;
+            System.out.printf("
+");
+        }
+    }
+}
