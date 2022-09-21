ec9ddd76add319848af6a77241c494f22e1d00ff
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-09-21 09:13:51 +0530

    import java.util.*; class Pat{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=1;i<=n;i++){             for(int j=1;j<=i;j++){                 System.out.printf("%d",j);             }             System.out.printf(" ");         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,17 +1,12 @@
 import java.util.*;
-class Main{
+class Pat{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n;
         n=sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j){
-                    System.out.printf("0");
-                }
-                else{
-                    System.out.printf("x");
-                }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.printf("%d",j);
             }
             System.out.printf("
 ");
