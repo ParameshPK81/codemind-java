5903c9a9c9809bc0ccd244a86f705f6d898a4c13
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-09-22 20:26:49 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);       int m,n;       n=sc.nextInt();       m=sc.nextInt();       if(n%2==0 || m%2==0){           System.out.println("Player 1");       }       else{            System.out.println("Player 2");       }       }     }

---
+++ Predict_the_winner_in_Coin_Game.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+      int m,n;
+      n=sc.nextInt();
+      m=sc.nextInt();
+      if(n%2==0 || m%2==0){
+          System.out.println("Player 1");
+      }
+      else{
+           System.out.println("Player 2");
+      }
+      }
+    }
