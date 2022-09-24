f8a3430c7b8bbf9ad0595c32422529f17fd9c74b
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-09-24 11:14:26 +0530

    import java.util.*; class Sub{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n,k=1,r;         long a,b;         n=sc.nextInt();         while(n>0){             a=sc.nextLong();             b=sc.nextLong();          System.out.print(a+b);          System.out.println();
             n--;
            }
        }
    }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Sub{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n,k=1,r;
+        long a,b;
+        n=sc.nextInt();
+        while(n>0){
+            a=sc.nextLong();
+            b=sc.nextLong();
+         System.out.print(a+b);
+         System.out.println();
+         
+         n--;
+        }
+    }
+}
