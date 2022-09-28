18ee188d6915255eb8ee84f09bc8882082bce841
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-09-28 06:52:12 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n,sum=0;         n = sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++){             a[i]=sc.nextInt();         }         int l,r;         l = sc.nextInt();         r = sc.nextInt();
            for(int i:a){
                if(l<=i && i<=r){
                    sum+=1;
                System.out.print(i+" ");
                }
            }
        
            if(sum==0){
                System.out.print(-1);
            }
        }
    }

---
+++ elements_in_between_A_and_B.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n,sum=0;
+        n = sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int l,r;
+        l = sc.nextInt();
+        r = sc.nextInt();
+     
+        for(int i:a){
+            if(l<=i && i<=r){
+                sum+=1;
+            System.out.print(i+" ");
+            }
+        }
+    
+        if(sum==0){
+            System.out.print(-1);
+        }
+    }
+}
