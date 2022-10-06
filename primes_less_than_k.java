09933cfccc27c3b77ca1d30ed5241f3e51f9fb32
Paramesh Karamsetty <97424433+ParameshPK81@users.noreply.github.com>
2022-10-06 19:38:41 +0530

    import java.util.Scanner; import java.lang.Math; class pir{     public static boolean prime(int n){          for(int j=2;j<=Math.sqrt(n);j++)             {                 if(n%j==0)                 {                   return false;                 }             }             return true;     }     public static void main(String args[]){
            Scanner sc=new Scanner(System.in);
            int n;
            n=sc.nextInt();
            int a[] = new int[n];
            for(int i=0;i<n;i++)
            {
                a[i] = sc.nextInt();
            }
            int k,c=0;
            k = sc.nextInt();
            for(int i:a){
                if(prime(i) && i!=1){
                    if(i<=k){
                        c++;
                    }
                }
            }
            System.out.print(c);
        }
    }

---
+++ primes_less_than_k.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.lang.Math;
+class pir{
+    public static boolean prime(int n){
+         for(int j=2;j<=Math.sqrt(n);j++)
+            {
+                if(n%j==0)
+                {
+                  return false;
+                }
+            }
+            return true;
+    }
+    public static void main(String args[]){
+      
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int a[] = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i] = sc.nextInt();
+        }
+        int k,c=0;
+        k = sc.nextInt();
+        for(int i:a){
+            if(prime(i) && i!=1){
+                if(i<=k){
+                    c++;
+                }
+            }
+        }
+        System.out.print(c);
+    }
+}
